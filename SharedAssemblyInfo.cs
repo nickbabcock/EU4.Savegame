ebd154cd6c646f307648edbf7212f50b294047f6
Nick Babcock <nbabcock19@hotmail.com>
2013-08-06 21:12:27 -0500

    Fix typos

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("EU4 Savegame")]
 [assembly: AssemblyDescription("EU4 savegame parser")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EU4.avegame")]
+[assembly: AssemblyProduct("EU4.Savegame")]
 [assembly: AssemblyCopyright("Copyright (C) Nick Babcock")]
 [assembly: AssemblyVersion("0.1.0.0")]
