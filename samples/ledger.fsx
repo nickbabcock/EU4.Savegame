0c5b7583f0f48a7f2ad7b41f0bcb30800552df7b
Nick Babcock <nbabcock19@hotmail.com>
2013-11-22 11:06:21 -0500

    Calculate relative statistics

--- samples/ledger.fsx
+++ samples/ledger.fsx
@@ -49,3 +49,60 @@ GreatestYearlyIncrease save.NationSizeStatistics
 GreatestYearlyIncrease save.ScoreStatistics
 |> ConcatWithReverse
 |> PrintLedger "Score"
+
+// Function calculates the relative rank that a country stands in a certain year.
+// It looks intimidating but really is a series of simple steps.
+// Step 1: Create tuple that that is (country, year, value)
+// Step 2: Group the set by year
+// Step 3: Each group is then grouped again by y-value (countries can tie)
+// Step 4: Find out how many countries are in front of each group and then
+//         add one to get the rank of all the countries in current group
+// Step 5: Throw away all information except for sequence of (name, date, rank)
+let RelativeRank (data:seq<LedgerData>) =
+    data
+    |> Seq.map (fun d ->
+        seq { for (x, y) in Seq.zip d.XData d.YData do yield (d.Name, x, y) }
+        |> Seq.filter (fun (_, _, y) -> y <> 0))
+    |> Seq.collect(fun s -> s)
+    |> Seq.groupBy (fun (_, date, _) -> date)
+    |> Seq.sortBy (fun (date, _) -> date)
+    |> Seq.map
+       (fun (date, col) ->
+           (date, col
+                  |> Seq.sortBy (fun (_,_,y) -> (~-) y)
+                  |> Seq.groupBy (fun (_, _, y) -> y)))
+    |> Seq.map
+       (fun (date, col) ->
+           (date, col
+                  |> Seq.zip (col
+                      |> Seq.map(fun (_, group) -> group |> Seq.length)
+                      |> Seq.scan (+) 0)
+                  |> Seq.collect(fun (i, (y, gr)) ->
+                      seq { for (n, x, y) in gr do yield (n, x, i+1) })))
+    |> Seq.collect snd
+
+// Return a sequence of tuples of (name, x) where x is the number of
+// years that the name was number 1 in the rankings
+let YearsOfFirst data =
+    data
+    |> Seq.groupBy(fun (name, _, _) -> name)
+    |> Seq.map (fun (name, group) ->
+               (name, group
+                      |> Seq.filter (fun (_,_,rank) -> rank = 1)
+                      |> Seq.length))
+    |> Seq.sortBy (fun (_, years) -> (~-) years)
+
+let firstIncome = YearsOfFirst (RelativeRank save.IncomeStatistics)
+let firstInflation = YearsOfFirst (RelativeRank save.InflationStatistics)
+let firstSize = YearsOfFirst (RelativeRank save.NationSizeStatistics)
+let firstScore = YearsOfFirst (RelativeRank save.ScoreStatistics)
+
+let zip4 s1 s2 s3 s4 =
+  Seq.map2 (fun (a,b)(c,d) ->a,b,c,d) (Seq.zip s1 s2)(Seq.zip s3 s4)
+
+printfn "----- Number of Years Being Ranked First -----"
+printfn "%-9s %-9s %-9s %-9s" "Income" "Inflation" "Size" "Score"
+zip4 firstIncome firstInflation firstSize firstScore
+|> Seq.take 10
+|> Seq.iter (fun ((n1, y1), (n2, y2), (n3, y3), (n4, y4)) ->
+    printfn "%-5s %3d %-5s %3d %-5s %3d %-5s %3d" n1 y1 n2 y2 n3 y3 n4 y4)
