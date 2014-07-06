cbac51a5ada70d998a898e78a4683e506c861069
Nick Babcock <nbabcock19@hotmail.com>
2014-07-06 10:36:29 -0400

    Add additional parsing to file

--- EU4.Stats/CountryStats.fs
+++ EU4.Stats/CountryStats.fs
@@ -4,7 +4,7 @@ open EU4.Savegame;
 open Types
 
 /// Calculates the desire for various AI countries to want something
-let highestAiPriorities (save:Savegame) (fn:Ai -> seq<AiPriority>) =
+let highestAiPriorities (save:Savegame) (fn:Ai -> seq<AiProvincePriority>) =
     save.Countries
     |> Seq.where (fun x -> x.NumOfCities > 0)
     |> Seq.collect (fun x -> fn x.Ai)
