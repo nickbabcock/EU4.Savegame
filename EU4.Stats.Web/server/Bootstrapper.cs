6f52edce329d5bf260de50f5fc4de254099f0626
Nick Babcock <nbabcock19@hotmail.com>
2014-07-11 21:10:17 -0500

    Helpful json error messages

---
+++ EU4.Stats.Web/server/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Responses;
+using Nancy.Serialization.JsonNet;
+using Nancy.TinyIoc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EU4.Stats.Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            // On error, send back a user friendly message and a developer friendly message in JSON.
+            // The Web UI is expecting "message" and "developer" but in C# we use capitals, we we get
+            // the best of both worlds by customizing a serializer that will do the conversion for us
+            var serializer = new JsonSerializer() { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+            pipelines.OnError.AddItemToEndOfPipeline((with, exception) =>
+                    new JsonResponse<ErrorMessage>(
+                        new ErrorMessage(exception),
+                            new JsonNetSerializer(serializer))
+                        .WithStatusCode(HttpStatusCode.InternalServerError));
+
+            base.RequestStartup(container, pipelines, context);
+        }
+    }
+}
---
+++ EU4.Stats.Web/server/ErrorMessage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EU4.Stats.Web
+{
+    /// <summary>
+    /// Class that will be serialized to the client on error.
+    /// It contains a user friendly and developer friendly
+    /// messages.
+    /// </summary>
+    public class ErrorMessage
+    {
+        public ErrorMessage(Exception e)
+        {
+            this.Error = new InnerError();
+            this.Error.Message = e.Message;
+            this.Error.Developer = e.Source + Environment.NewLine + e.StackTrace;
+        }
+
+        /// <summary>
+        /// Gets or sets the wrapped messages in an overall "error" object
+        /// </summary>
+        public InnerError Error { get; set; }
+
+        public class InnerError
+        {
+            /// <summary>
+            /// Gets or sets the user friendly message that is to 
+            /// be displayed in the interface to let the client
+            /// know that something is wrong
+            /// </summary>
+            public string Message { get; set; }
+
+            /// <summary>
+            /// Gets or sets the developer friendly message that should
+            /// aid in debugging given that there is a problem with the
+            /// service
+            /// </summary>
+            public string Developer { get; set; }
+        }
+    }
+}
