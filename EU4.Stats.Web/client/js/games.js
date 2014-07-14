6e3d9c2d084dd7a8baa9df1be56ec97178798aec
Nick Babcock <nbabcock19@hotmail.com>
2014-07-14 10:28:12 -0500

    Formatting changes in games

--- EU4.Stats.Web/client/js/games.js
+++ EU4.Stats.Web/client/js/games.js
@@ -1,13 +1,31 @@
+var tableTools = {
+    sSwfPath: '//cdn.jsdelivr.net/jquery.datatables/1.10.0/plugins/tabletools/swf/copy_csv_xls.swf',
+    aButtons: ['copy', 'csv', 'xls']
+};
+
 $(function() {
-    $('table.paging').dataTable();
-    $('table').not('table.paging').dataTable({
+
+    $('#leaderReport').dataTable({
+        dom: 'T<"clear">lfrtip',
+        tableTools: tableTools
+    });
+
+    $('#navalWars,#landWars,#rivalries').dataTable({
         paging: false,
         searching: false,
         info: false,
         order: [],
         dom: 'T<"clear">lfrtip',
-        tableTools: { 
-            sSwfPath: "//cdn.jsdelivr.net/jquery.datatables/1.10.0/plugins/tabletools/swf/copy_csv_xls_pdf.swf"
-        }
+        tableTools: tableTools 
+    });
+
+    $('#correlation').dataTable({
+        paging: false,
+        searching: false,
+        ordering: false,
+        info: false,
+        order: [],
+        dom: 'T<"clear">lfrtip',
+        tableTools: tableTools 
     });
 });
