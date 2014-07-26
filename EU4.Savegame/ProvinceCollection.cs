528e9675ed26566393b7cb89a114fdde347716ee
Nick Babcock <nbabcock19@hotmail.com>
2014-07-26 08:56:31 -0500

    Province collection ensure add and get implement
    The ProvinceCollection class now ensures that a province added has the
    id, else an exception is thrown. A get function has been reinstated to
    provided O(1) access times to a particular province.

--- EU4.Savegame/ProvinceCollection.cs
+++ EU4.Savegame/ProvinceCollection.cs
@@ -9,33 +9,51 @@ namespace EU4.Savegame
 {
     public class ProvinceCollection : IEnumerable<Province>, IParadoxRead, IParadoxWrite
     {
-        private IList<Province> provinces;
+        private IList<Province> provinces = new List<Province>(ProvincesEstimate);
 
+        // Last checked in 1.7 there were 2025 provinces, we initialize our list
+        // of provinces to the next highest power of two so that the list
+        // doesn't need to resize as many times.
         private const int ProvincesEstimate = 2048;
 
-        public ProvinceCollection()
-        {
-            this.provinces = new List<Province>(ProvincesEstimate);
-        }
-
+        /// <summary>
+        /// Adds a given province to the collection and ensures that it
+        /// follows suit with an increasing id
+        /// </summary>
+        /// <param name="prov">Province to add to the collection</param>
+        /// <exception cref="ApplicationException">
+        /// If the added province doesn't have the expected id
+        /// </exception>
         public void Add(Province prov)
         {
-            this.provinces.Add(prov);
+            provinces.Add(prov);
+            if (prov.Id != provinces.Count)
+                throw new ApplicationException("Added province with an id of "
+                    + prov.Id + " but expected a province with an id of "
+                    + provinces.Count);
+        }
+
+        /// <summary>
+        /// Returns a province with the given province id in O(1) time
+        /// </summary>
+        public Province Get(int provinceId)
+        {
+            return provinces[provinceId - 1];
         }
 
         public IEnumerator<Province> GetEnumerator()
         {
-            return this.provinces.GetEnumerator();
+            return provinces.GetEnumerator();
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
-            return this.provinces.GetEnumerator();
+            return provinces.GetEnumerator();
         }
 
         public void TokenCallback(ParadoxParser parser, string token)
         {
-            this.provinces.Add(parser.Parse(new Province(-int.Parse(token))));
+            provinces.Add(parser.Parse(new Province(-int.Parse(token))));
         }
 
         public void Write(ParadoxStreamWriter writer)
